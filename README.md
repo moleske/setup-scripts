7294cc464bee90f7ffe64c1d430ab4e3fd692624
Michael Oleske <moleske@pivotal.io>
2016-09-12 14:42:31 -0700

    Update homebrew setup to be more complete
    closes #2

--- README.md
+++ README.md
@@ -33,12 +33,20 @@ Adds Following Git Aliases
 co, br, ci, st, lola
 
 ####Mac
-Try [NMahoney's](https://github.com/nmahoney-pivotal/scripts) script for basics
-This has extra for development
+Inspired by [NMahoney's](https://github.com/nmahoney-pivotal/scripts) script for basics
+This has all of NMahoney's plus extra for development
+**This assumes [Homebrew](http://brew.sh) is installed already**
 ```
 ruby homebrew-set-up.rb
 ```
 Installs The Following
+- tree
+- google-chrome
+- flycut
+- iterm2
+- shiftit
+- flux
+- Bash it
 - vim
 - git
 - git-duet
@@ -48,4 +56,12 @@ Installs The Following
 - node
 
 Adds Following Git Aliases
-dci, drv, dmg, lola
+co, br, ci, st, dci, drv, dmg, lola
+
+**Note:**  Disables [Brew Analytics](https://git.io/brew-analytics)
+
+####vimrc & inputrc
+Copy them yourself to $HOME you lazy bum (maybe someday I'll do something about that)
+
+####User Initials in Git Info
+Copy `add_user_initials_to_git_info.bash` to `$HOME/.bash_it/custom` you bum (again, maybe someday I'll yadda yadda)
--- homebrew-set-up.rb
+++ homebrew-set-up.rb
@@ -1,6 +1,36 @@
 #! /usr/bin/env ruby
+puts 'Disable Brew Analytics'
+`brew analytics off`
+
+puts 'Installing Homebrew Cask...'
+`brew install cask`
+
+puts 'Installing tree...'
+`brew install tree`
+
+puts 'Installing Google Chrome...'
+`brew cask install google-chrome`
+
+puts 'Installing Flycut...'
+`brew cask install flycut`
+
+puts 'Installing iTerm...'
+`brew cask install iterm2`
+
+puts 'Installing ShiftIt...'
+`brew cask install shiftit`
+
+puts 'Installing f.lux...'
+`brew cask install flux`
+
+puts 'Installing Bash it...'
+`git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it`
+`~/.bash_it/install.sh`
+`echo "export BASH_IT_THEME='envy'" >> ~/.bashrc`
+
 puts 'Installing VIM...'
 `brew install vim`
+`git config --global core.editor /usr/bin/vim`
 
 puts 'Installing git...'
 `brew install git`
@@ -9,11 +39,17 @@ puts 'Installing git-duet...'
 `brew tap git-duet/tap`
 `brew install git-duet`
 
+puts 'Adding git aliases (co, br, ci, and st)...'
+`git config --global alias.co checkout`
+`git config --global alias.br branch`
+`git config --global alias.ci commit`
+`git config --global alias.st status`
+
 puts 'Adding git aliases dci, drv, dmg, lola'
 `git config --global alias.dci duet-commit`
 `git config --global alias.drv duet-revert`
 `git config --global alias.dmg duet-merge`
-`git config --global alias.lola log --graph --decorate --pretty=oneline --abbrev-commit --all`
+`git config --global alias.lola "log --graph --decorate --pretty=oneline --abbrev-commit --all"`
 
 puts 'Installing CF CLI'
 `brew tap cloudfoundry/tap`
