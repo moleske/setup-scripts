4d49b59f5895039606ca74f1ae7da29fee0edfaa
M. Oleske <moleske@users.noreply.github.com>
2017-07-03 11:24:35 -0700

    Add ditto and fix string

--- choco-set-up.ps1
+++ choco-set-up.ps1
@@ -5,7 +5,7 @@ echo 'Installing Chocolatey'
 iex ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1'))
 
 echo 'Installing Base'
-cinst 7zip ccleaner keepass launchy notepadplusplus rainmeter sumatrapdf virtualbox VirtualCloneDrive vlc -y
+cinst 7zip ccleaner ditto keepass launchy notepadplusplus rainmeter sumatrapdf virtualbox VirtualCloneDrive vlc -y
 
 echo 'Installing Development Tools'
 cinst consolez git cloudfoundry-cli intellijidea-ultimate jdk8 nodejs poshgit postgresql python2 ruby -y
@@ -18,4 +18,4 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
-git config --global alias.lola log --graph --decorate --pretty=oneline --abbrev-commit --all
+git config --global alias.lola "log --graph --decorate --pretty=oneline --abbrev-commit --all"
